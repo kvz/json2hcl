026d0d2b73b4fde694c4e4430c70dc905078b921
Marius <maerious@gmail.com>
2016-09-05 22:08:16 +0200

    Implement proper error handling and output

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
-  "os"
-  "io/ioutil"
-  "github.com/hashicorp/hcl/json/parser"
-  "github.com/hashicorp/hcl/hcl/printer"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/hashicorp/hcl/hcl/printer"
+	"github.com/hashicorp/hcl/json/parser"
 )
 
 func main() {
-  input, _ := ioutil.ReadAll(os.Stdin)
+	if err := convert(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
 
-  ast, err := parser.Parse([]byte(input))
-  if err != nil {
-    panic(err)
-  }
+func convert() error {
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return fmt.Errorf("unable to read from stdin: %s", err)
+	}
 
-  err = printer.Fprint(os.Stdout, ast)
-  if err != nil {
-    panic(err)
-  }
+	ast, err := parser.Parse([]byte(input))
+	if err != nil {
+		return fmt.Errorf("unable to parse JSON: %s", err)
+	}
+
+	err = printer.Fprint(os.Stdout, ast)
+	if err != nil {
+		return fmt.Errorf("unable to print HCL: %s", err)
+	}
+
+	return nil
 }
