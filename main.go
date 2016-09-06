2eba0bb233a6560c63dfb97d71694c30d2f55ad6
Kevin van Zonneveld <vanzonneveld@gmail.com>
2016-09-06 11:50:33 +0200

    Hcl2json (#1)
    * First swing at hcl2json
    
    * Unmarshal HCL into interface{} and indent JSON
    
    * Also test the reverse, making sure that works as well as json2hcl returns valid hcl
    
    * Whitespace

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 
+	hcl "github.com/Acconut/hcl"
 	"github.com/Acconut/hcl/hcl/printer"
-	"github.com/Acconut/hcl/json/parser"
+	jsonParser "github.com/Acconut/hcl/json/parser"
 )
 
 // VERSION is what is returned by the `-v` flag
@@ -15,25 +17,55 @@ var VERSION = "v0.0.4"
 
 func main() {
 	version := flag.Bool("version", false, "Prints current app version")
+	reverse := flag.Bool("reverse", false, "Input HCL, output JSON")
 	flag.Parse()
 	if *version {
 		fmt.Println(VERSION)
 		os.Exit(0)
 	}
 
-	if err := convert(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	if *reverse {
+		if err := toJSON(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(0)
+		}
+	} else {
+		if err := toHCL(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 }
 
-func convert() error {
+func toJSON() error {
 	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		return fmt.Errorf("unable to read from stdin: %s", err)
 	}
 
-	ast, err := parser.Parse([]byte(input))
+	var v interface{}
+	err = hcl.Unmarshal(input, &v)
+	if err != nil {
+		return fmt.Errorf("unable to parse HCL: %s", err)
+	}
+
+	json, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return fmt.Errorf("unable to marshal json: %s", err)
+	}
+
+	fmt.Println(string(json))
+
+	return nil
+}
+
+func toHCL() error {
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return fmt.Errorf("unable to read from stdin: %s", err)
+	}
+
+	ast, err := jsonParser.Parse([]byte(input))
 	if err != nil {
 		return fmt.Errorf("unable to parse JSON: %s", err)
 	}
